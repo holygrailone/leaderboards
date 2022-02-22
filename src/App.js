5e41fe01cf3d70f27470f052e4219088665abd12
jyjyj <jiaying.goh92@gmail.com>
2022-02-22 22:04:01 +0800

    first cut for styling leaderboard with material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import './App.css';
-import { render } from 'react-dom';
-import React,{useState,useEffect}  from 'react';
-import SortableTable from 'react-sortable-table';
+import "./App.css";
+import React from "react";
 
-import Table from './Table.js';
+// import Table from './Table.js';
+import LeaderboardTable from "./LeaderboardTableV2.js";
+import Footer from "./Footer";
 
 function App() {
-
   return (
     <div className="App">
-      <header style={{marginTop: '55px'}}>
-        <img src="https://holygrail.one/holygrailonegame.png" alt="Holy Grail" className="logo" />
-        <h1>
-          Leaderboards
-        </h1>
-        <p>These Holy Grail Leaderboards refresh statistics every ~30 minutes.</p>
+      <header style={{ marginTop: "55px" }}>
+        <img
+          src="https://holygrail.one/holygrailonegame.png"
+          alt="Holy Grail"
+          className="logo"
+        />
+        <h1>Leaderboards</h1>
+        <p>
+          These Holy Grail Leaderboards refresh statistics every ~30 minutes.
+        </p>
         <br />
       </header>
-      <Table />
-      <div className="App-body">      
-      </div>
+      <LeaderboardTable />
+
+      <div style={{ height: 70 }} />
+
+      <Footer />
+      <div className="App-body" />
     </div>
   );
 }
