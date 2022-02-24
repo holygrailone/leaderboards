d5c065f8828ea34bac22d81abe733f16d65acc92
jyjyj <jiaying.goh92@gmail.com>
2022-02-24 21:46:13 +0800

    add ThemeProvider + count num legends minted

---
+++ src/Theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const AppTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#f59e1d",
+    },
+    background: {
+      main: "#3d3830",
+    },
+    tonalOffset: 0.5,
+  },
+});
+
+export default AppTheme;
